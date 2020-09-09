dd7fa5dfae282d9671b056bb63bb27144e6a4e2b
JeremiasCastillo25 <jerecastillo25@gmail.com>
2020-09-09 20:34:34 -0300

    soluciono problema

--- sitepqn/routes/users.js
+++ sitepqn/routes/users.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-const controller = require('../controllers/usersController');
+const controller = require('../controllers/usersControllers');
 const sessionUserCheck = require('../middlewares/middlewareSessionUserCheck');
 
-const upImageAvatar = require('../middlewares/midllewareUpImageAvatar');
+const upImageAvatar = require('../middlewares/middelwareUpImageAvatar');
 
 const registerValidator = require('../validations/registerValidator');
 const loginValidator = require('../validations/loginValidator');
+const usersControllers = require('../controllers/usersControllers');
 
 router.get('/registro',controller.register);
-router.post('/registro',upImageAvatar.any(),registerValidator, controller.processRegister);
+router.post('/registro',upImageAvatar.any(),registerValidator, usersControllers.processRegister);
 
-router.get('/registro',controller.login);
-router.post('/registro',loginValidator,controller.processLogin);
+router.get('/registro',usersControllers.login);
+router.post('/registro',loginValidator,usersControllers.processLogin);
 
-router.get('/profile',sessionUserCheck, controller.profile);
+router.get('/profile',sessionUserCheck, usersControllers.profile);
 
-router.get('/logout',controller.logout);
+router.get('/logout',usersControllers.logout);
 
 
 
