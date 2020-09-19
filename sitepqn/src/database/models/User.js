0391cac46f9372bc7dab8bb589df90d26a277ac7
CaroBarila <carolinabarila@hotmail.com>
2020-09-19 01:14:18 -0300

    carpeta src, instalé sequelize e inicié los modelos

---
+++ sitepqn/src/database/models/User.js
@@ -0,0 +1,63 @@
+module.exports = (sequelize,dataTypes) => {
+    
+    let alias = "Users";
+    let cols = {
+        id:{
+            type:dataTypes.INTEGER.UNSIGNED,
+            primaryKey:true,
+            autoIncrement:true,
+            allowNull:false
+        },
+        nombre:{
+            type: dataTypes.STRING(45),
+            allowNull:false
+        },
+        apellido:{
+            type: dataTypes.STRING(45),
+            allowNull:false
+        },
+        email:{
+            type: dataTypes.STRING(45),
+            allowNull:false,
+            unique:true
+        },
+        pass:{
+            type: dataTypes.STRING(100),
+            allowNull:false
+        },
+        dni:{
+            type: dataTypes.CHART(8),
+            unique:true
+        },
+        avatar:{
+            type: dataTypes.STRING(100)
+        },
+        direccion:{
+            type: dataTypes.STRING(255),
+            unique:true
+        },
+        ciudad:{
+            type: dataTypes.STRING(255),
+            allowNull:false
+        },
+        provincia:{
+            type: dataTypes.STRING(100),
+            allowNull:false
+        },
+        rol:{
+            type: dataTypes.STRING(45),
+            allowNull:false
+        },
+    }
+
+    let config = {
+        tableName: "users",
+        timestamps:true,
+        underscored:true
+
+    }
+
+    const User = sequelize.define(alias,cols,config);
+
+    return User;
+}
