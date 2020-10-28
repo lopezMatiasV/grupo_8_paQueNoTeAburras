64e08a61048ff2270d451aa1fd8d91308c421396
CaroBarila <carolinabarila@hotmail.com>
2020-10-27 23:40:01 -0300

    Validaciones del Front extension de Imagenes

--- sitepqn/public/javascripts/formCargaV.js
+++ sitepqn/public/javascripts/formCargaV.js
@@ -17,21 +17,26 @@ window.addEventListener('load',function(){
     let inputSku = qs('#sku');
     let inputNombre = qs('#nombre');
     let inputPrice = qs('#precio');
-    let inputCategory= qs('#categoria');
+    let selectCategory= qs('#categoria'); //usé un select, no puede quedar vacía
     let inputSCategory = qs('#subCategoria');
-    let inputSeccion = qs('#seccion');
-    let inputDiscount = qs('#discount');
-    let selectDescrip= qs('#exampleFormControlTextarea1');
+    let selectSeccion = qs('#seccion'); // usé un select, esta puede quedar vacía
+    let inputDiscount = qs('#discount'); //puede quedar vacío
+    let textareaDescrip= qs('#exampleFormControlTextarea1'); 
     let inputFoto = qs('#foto');
     //let vistaPrevia = qs('#vistaPrevia');
 
 //expresiones regulares
 
 let errores = { } //objeto literal de errores vacio para que se vaya llenando
-let regExSKU = /^[0-9]{6}$/; // debe comenzar en 3XXXXX
-let regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/; //ver/
+let regExSKU = /^[0-9]{6}$/; 
+let regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/; 
 let regExPrice = /^[0-9]+(,[0-9]+)?$/;
-let regExTXT = /^(?=.*\W)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // cualquier valor alfabetico?ni numeros ni caracteres especiales
+let regExTXT1 = /^(?=.*\W)(?=.*[a-z])(?=.*[A-Z]).{6,45}$/; 
+let regExTXT = /^(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+let regExExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
+let regExtTXT2="[a-zA-Z ]{2,254}"
+//Te permitirá ingresar letras de la hasta la z, un con un mínimo de 2 caracteres y máximo 254//
+
 
 //las validaciones del front las refleja sobre cada uno de los inputs
 // --> validaciones
@@ -96,7 +101,7 @@ inputPrice.addEventListener('blur', function(){
     }
 })
 
-/*inputCategory.addEventListener('blur', function(){
+selectCategory.addEventListener('blur', function(){
     switch (true) {
         case this.value.length == 0:
             errores.categoria = "La Categoria es obligatoria";
@@ -114,18 +119,18 @@ inputPrice.addEventListener('blur', function(){
             errorCategoria.innerHTML = ""  // lo vacio
             break;
     }
-})*/
+})
 
 inputSCategory.addEventListener('blur', function(){
     switch (true) {
         case this.value.length == 0:
-            errores.eccion = "El campo es obligatorio";
-            errorSubcategoria.innerHTML = errores.eccion
+            errores.subCategoria = "El campo es obligatorio";
+            errorSubcategoria.innerHTML = errores.subCategoria
             this.classList.add('is-invalid')
             break;
             case this.value.length <= 5: //debe tener al menos 6 letras 
-        errores.eccion = "Debes escribir una Subcategoria válida";
-        errorSubcategoria.innerHTML = errores.eccion
+        errores.seccion = "Debes escribir una Subcategoria válida";
+        errorSubcategoria.innerHTML = errores.subCategoria
         this.classList.add('is-invalid')
         break;
         default:
@@ -136,23 +141,93 @@ inputSCategory.addEventListener('blur', function(){
     }
 })
 
-function fileValidation(){
-    var fotoPath = inputFoto.value;
-    var allowedExtensions = /(.jpg|.jpeg|.png|.gif)$/i;
-    if(!allowedExtensions.exec(fotoPath)){
-        alert('Por favor revise la extensión del archivo .jpeg/.jpg/.png/.gif solo.');
-        inputFoto.value = '';
-        return false;
-    }else{
-        //Image preview
-        if (inputFoto.files && inputFoto.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                document.getElementById('imagePreview').innerHTML = '<img src="'+e.target.result+'"/>';
-            };
-            reader.readAsDataURL(inputFoto.files[0]);
-        }
+selectSeccion.addEventListener('blur', function(){
+    switch (true) {
+        case this.value.length == 0:
+            errores.seccion = "La Sección es obligatoria";
+            errorSeccion.innerHTML = errores.seccion  //para las etiquetas span del index.ejs (error nombre le voy a agregar el string errores.nombre)
+            this.classList.add('is-invalid') //estoy agregando la clase is-invalid, si es que hay un error.
+            break;
+    case this.value.length <= 5: //debe tener al menos 6 letras
+        errores.seccion = "El campo sección debe estar seleccionado incluso en opcion vacío";
+        errorSeccion.innerHTML = errores.seccion
+        this.classList.add('is-invalid')
+        break;
+        default:
+            this.classList.remove('is-invalid'); //sobre el mismo elemento si hubiera habido un error lo voy a remover y le voy a agregar
+            this.classList.add('is-valid');
+            errorSeccion.innerHTML = ""  // lo vacio
+            break;
     }
-}
+})
 
+inputDiscount.addEventListener('blur', function(){
+    switch (true) {
+        case this.value.length == 0:
+            errores.discount = "El campo del descuento es obligatorio,  de no aplicar descuento deberá validar la opción cero";
+            errorDiscount.innerHTML = errores.discount
+            this.classList.add('is-invalid')
+            break;
+    case !regExPrice.test(this.value):  
+        errores.discount = "Debes escribir un valor aplicable al descuento";
+        errorPrecio.innerHTML = errores.discount
+        this.classList.add('is-invalid')
+        break;
+        default:
+            this.classList.remove('is-invalid');
+            this.classList.add('is-valid');
+            errorDiscount.innerHTML = ""  // lo vacio
+            break;
+    }
+})
+
+/*textareaDescrip.addEventListener('blur', function(){
+    switch (true) {
+        case this.value.length == 0:
+            errores.exampleFormControlTextarea1 = "El campo de la descripción es obligatorio";
+            errorDescripcion.innerHTML = errores.exampleFormControlTextarea1
+            this.classList.add('is-invalid')
+            break;
+    case !regExTXT.test(this.value):  
+        errores.exampleFormControlTextarea1 = "Debes escribir una Descripción de al menos 20 caracteres";
+        errorDescripcion.innerHTML = errores.exampleFormControlTextarea1
+        this.classList.add('is-invalid')
+        break;
+        default:
+            this.classList.remove('is-invalid');
+            this.classList.add('is-valid');
+            errorDescripcion.innerHTML = ""  // lo vacio
+            break;
+    }
+})*/
+
+
+
+
+inputFoto.addEventListener('change',function(e){
+    switch (true) {
+        case !regExExtensions.exec(this.value) :
+            errores.foto = "Solo imagenes con extension jpg, jpeg, png, o gif"
+            errorFoto.innerHTML = errores.foto;
+            this.classList.add('is-invalid')
+            this.value = '';
+            vistaPrevia.src = "";
+        break
+        default:
+            this.classList.remove('is-invalid');
+            this.classList.add('is-valid');
+            errorFoto.innerHTML = "";
+            // Creamos el objeto de la clase FileReader
+            let reader = new FileReader();
+            // Leemos el archivo subido y se lo pasamos a nuestro fileReader
+            reader.readAsDataURL(e.target.files[0]);
+            // Le decimos que cuando este listo ejecute el código interno
+            reader.onload = function(){
+            vistaPrevia.src = reader.result;
+            };
+            this.classList.remove('is-invalid');
+            this.classList.add('is-valid');
+            errorFoto.innerHTML = "";
+        }
+    })
 })
